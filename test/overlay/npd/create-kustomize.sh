0307fe767db3d3127b1dafcb379cbe258b1ac68f
Nattapongsiri Narmkul <nattapongsiri@sirisoft.co.th>
2025-01-08 11:54:23 +0700

    publish branch single-app

--- test/overlay/npd/create-kustomize.sh
+++ test/overlay/npd/create-kustomize.sh
@@ -22,7 +22,7 @@ apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 namespace: $namespace
 
-bases:
+resources:
   - ../../../base
 EOF
 
---
+++ test/overlay/npd/nut-4/create_patch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# allow namespace list
+NAMESPACES=(
+  nut-5
+)
+
+# create allow namespace
+for NAMESPACE in "${NAMESPACES[@]}"; do
+  cat <<EOF > "allow-namespace-${NAMESPACE}.yml"
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: allow-namespace-${NAMESPACE}
+spec:
+  podSelector: {}
+  ingress:
+    - from:
+      - namespaceSelector:
+          matchLabels:
+            app.kubernetes.io/instance: ${NAMESPACE}
+EOF
+
+  # update kustomization.yml
+  if ! grep -q "allow-namespace-${NAMESPACE}.yml" "kustomization.yml"; then
+    echo "  - allow-namespace-${NAMESPACE}.yml" >> "kustomization.yml"
+  fi
+done
+
+echo "Done!"
